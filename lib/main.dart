ca56e0c52726bec24e92829e6512ebe081e1f4e5
Timus12345 <reviobandela51451@gmail.com>
2025-06-12 09:38:14 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:stroitel/login_screen/login_screen.dart';
+import 'package:stroitel/password_screen/password_screen.dart';
+import 'package:stroitel/profile_screen/profile_screen.dart';
+import 'package:stroitel/settings_screen/settings_screen.dart';
+
+
+void main() async {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const login_screen(),
+      '/pass': (context) => const password_screen(),
+      '/profile': (context) => const profile_screen(),
+      '/settings': (context) => const settings_screen()
+    },
+  ));
+}
